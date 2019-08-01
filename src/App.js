23bcb695a62e8b8923ad43b65d3131152a1fcdf7
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-08-01 17:25:22 +0200

    add header component and header logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import "./pages/homepage/homepage.styles.scss";
 
+import { Route, Switch } from "react-router-dom";
+
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import React from "react";
-import { Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
