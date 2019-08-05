87b809b7b51747a169f73f2c0fb81664d9756969
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-08-05 17:50:47 +0200

    create selector to get cart total price

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,40 @@
 import "./checkout.component.scss";
 
+import {
+  selectCartItems,
+  selectCartTotalPrice
+} from "../../redux/cart/cart.selectors";
+
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-const CheckoutPage = () => <div>CHECKOUT PAGE</div>;
+const CheckoutPage = ({ cartItems, totalPrice }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Description</span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
+    </div>
+    {cartItems.map(cartItem => cartItem.name)}
 
-export default CheckoutPage;
+    <div className="total-price">TOTAL: €{totalPrice}</div>
+  </div>
+);
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  totalPrice: selectCartTotalPrice
+});
+export default connect(mapStateToProps)(CheckoutPage);
