87441e51139d3c471eb6af6c3ef439c5460abdf1
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-08-05 13:41:42 +0200

    implement itemCount selector to get the total amount of items in the cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,18 +5,25 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 import { setCartVisibility } from "../../redux/cart/cart.actions";
 
-const CartIcon = ({ setCartVisibility }) => {
+const CartIcon = ({ setCartVisibility, itemCount }) => {
   return (
     <div onClick={() => setCartVisibility()} className="cart-icon">
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
 const mapDispatchToProps = dispatch => ({
   setCartVisibility: () => dispatch(setCartVisibility())
 });
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  )
+});
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
