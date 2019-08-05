eaf827a1dfd53b390684914e0f3a3a5bd3d7e366
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-08-05 13:14:04 +0200

    implement add multiple items to cart functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { CartActionTypes } from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   showCart: false,
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload.item]
+        cartItems: addItemToCart(state.cartItems, action.payload.item)
       };
     default:
       return state;
