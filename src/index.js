79415425fdbfad2aa489da3086268097c85a5691
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-08-05 18:56:44 +0200

    persist cart items in local storage with redux-persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import "./index.css";
 
+import { persistor, store } from "./redux/store";
+
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
-import store from './redux/store';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-  	</Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,17 @@
 import cartReducer from "./cart/cart.reducer";
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
-export default combineReducers({ user: userReducer, cart: cartReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer
+});
+export default persistReducer(persistConfig, rootReducer);
