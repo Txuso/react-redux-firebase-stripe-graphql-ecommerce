64edec3faaaf02b58f2b4cb0dc6fdfaaddfb12e2
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-08-15 16:54:30 +0200

    normalise shop data object and style shop collection items

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,15 +1,25 @@
-import './collection.component.scss';
+import "./collection.component.scss";
 
-import React from 'react';
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
-const CollectionPage = ({ match }) => {
-	console.log('ksasaassa', match.params);
-	debugger;
-	return (
-		<div className="collection-page">
-			<h2>COLLECTION PAGE</h2>
-		</div>
-	);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,14 @@
-import { createSelector } from 'reselect';
+import { createSelector } from "reselect";
 
-const selectShop = (state) => state.shop;
+const selectShop = state => state.shop;
 
-export const selectShopCollections = createSelector([ selectShop ], (shop) => shop.collections);
+export const selectShopCollections = createSelector(
+  [selectShop],
+  shop => shop.collections
+);
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectShopCollections],
+    collections => collections[collectionUrlParam]
+  );
