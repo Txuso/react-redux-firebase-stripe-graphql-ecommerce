f95f597ebba72cbb1513da28e9babed9343918c5
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-08-16 15:57:00 +0200

    apply spinner higher order component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,10 +7,16 @@ import CollectionPage from "../collection/collection.component";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import React from "react";
 import { Route } from "react-router-dom";
+import WithSpinner from "../../components/with-spinner/with-spinner";
 import { connect } from "react-redux";
 import { updateCollections } from "../../redux/shop/shop.actions";
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionsPageWithSpinner = WithSpinner(CollectionPage);
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -20,17 +26,27 @@ class ShopPage extends React.Component {
     collectionRef.onSnapshot(snapShot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={props => (
+            <CollectionsPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
