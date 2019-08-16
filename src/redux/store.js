7ff6f3d0955f514c84f7c7f534f0f422ad4779d1
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-08-16 11:04:42 +0200

    remove redux logger from production version

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
