a9f243e1e380605ce415f5fdf181f4de9f798d32
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-10-10 12:42:41 +0200

    add styles to main home page

--- src/components/header/header.styled.jsx
+++ src/components/header/header.styled.jsx
@@ -1,26 +1,41 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
-  display: flex;
-  margin-bottom: 25px;
-  width: 100%;
-  height: 70px;
-  justify-content: space-between;
+	display: flex;
+	margin-bottom: 25px;
+	width: 100%;
+	height: 70px;
+	justify-content: space-between;
+
+	@media screen and (max-width: 800px) {
+		height: 60px;
+		padding: 10px;
+		margin-bottom: 20px;
+	}
 `;
 
 export const LogoContainer = styled(Link)`
   padding: 25px;
   width: 70px;
   height: 100%;
+
+  @media screen and (max-width: 800px) {
+		width: 50px;
+		padding: 0;
+	}
 `;
 
 export const OptionsContainer = styled.div`
-  display: flex;
-  width: 50%;
-  height: 100%;
-  align-items: center;
-  justify-content: flex-end;
+	display: flex;
+	width: 50%;
+	height: 100%;
+	align-items: center;
+	justify-content: flex-end;
+
+	@media screen and (max-width: 800px) {
+		width: 80%;
+	}
 `;
 
 export const OptionLink = styled(Link)`
